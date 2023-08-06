587f86e5206a72bad48187b83465fbc119b5072d
cnyt-y <56654010+cnyt-y@users.noreply.github.com>
2023-08-06 15:35:33 +0300

    dosya düzenlendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import './app.css';
+import { useState } from 'react';
+import './App.css';
+import{ Users } from "./users.js"
 
 function App() {
-    
+    const [query,setQuery] = useState("")
+
+    console.log(query)
     return (
-        <>
-            
-        </>
+        <div className='app'>
+            <input 
+                type="text" 
+                placeholder='Search...' 
+                className="search" 
+                onChange={(e) => setQuery(e.target.value)} 
+            />
+
+            <ul className="list">
+                {Users.map(user=>(
+                    <li key={user.id} className="listItem">{user.first_name}</li>
+                ))}
+            </ul>   
+        </div>
     )
 }
 
